06e26e2113e6d75ed43c10261e0c03b53ea7e033
Maryam Haghgou <artmary.ir@gmail.com>
2021-01-18 01:56:16 +0330

    datepicker in progress.

--- app/frontend/components/DateTimePicker.vue
+++ app/frontend/components/DateTimePicker.vue
@@ -1,35 +1,167 @@
 <template>
-  <v-dialog v-model="dialog" persistent max-width="290">
-    <template v-slot:activator="{ on, attrs }">
-      <v-btn color="primary" dark v-bind="attrs" v-on="on"> Open Dialog </v-btn>
-    </template>
-    <v-card>
-      <v-card-title class="headline">
-        Use Google's location service?
-      </v-card-title>
-      <v-card-text
-        >Let Google help apps determine location. This means sending anonymous
-        location data to Google, even when no apps are running.</v-card-text
-      >
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn color="green darken-1" text @click="dialog = false">
-          Disagree
-        </v-btn>
-        <v-btn color="green darken-1" text @click="dialog = false">
-          Agree
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+  <div>
+    <v-text-field
+      v-model="formatedDate"
+      :label="placeholder"
+      readonly
+      clearable
+      outlined
+      :dense="dense"
+      :prepend-inner-icon="icon"
+      @click:clear="clearDate"
+      @click.stop="dialog = true"
+    ></v-text-field>
+    <v-dialog v-model="dialog" width="500">
+      <v-card>
+        <v-card-title class="headline grey lighten-2">
+          {{ dialogDetail.title }}
+        </v-card-title>
+
+        <v-card-text class="pa-0 ma-0">
+          <!-- head  -->
+          <v-simple-table fixed-header class="pa-0 ma-0">
+            <template v-slot:default>
+              <thead>
+                <tr>
+                  <th
+                    v-for="head in dialogDetail.generatedTable.head"
+                    :key="head.name"
+                    class="text-center grey darken-3"
+                  >
+                    <span :class="head.weekend ? 'red--text' : 'white--text'">
+                      {{ head.narrow }}
+                    </span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="(week, i) in dialogDetail.generatedTable.weeks"
+                  :key="i"
+                  class="text-center"
+                >
+                  <td v-for="day in week" :key="day.dayLocale">
+                    {{ day.dayLocale }}
+                  </td>
+                </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+          <!-- <v-row class="grey darken-3">
+            <v-col
+              v-for="head in dialogDetail.generatedTable.head"
+              :key="head.name"
+              class="text-center"
+            >
+              <span class="white--text">
+                {{ head.narrow }}
+              </span>
+            </v-col>
+          </v-row> -->
+          <!-- week -->
+          <!-- <v-row class="py-2 grey lighten-4">
+            <v-col
+              v-for="day in dialogDetail.generatedTable.weeks"
+              :key="day.name"
+              cols="12"
+              class="text-center"
+            >
+              <v-row class="pa-0" cols="12">
+                <v-col v-for="i in day" :key="i.name" class="pa-0 text-center">
+                  <v-btn icon class="my-1">
+                    {{ i.dayLocale }}
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row> -->
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" text @click="dialog = false"> I accept </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
+import { AasaamDateTime } from '@aasaam/date-time';
 export default {
   name: 'DateTimePicker',
-  created() {},
   props: {
-    dateTime: Date,
+    dateTime: {
+      type: Date,
+      default: undefined,
+      required: false,
+    },
+    lang: {
+      type: String,
+      default: 'fa',
+      required: true,
+    },
+    icon: {
+      type: String,
+      default: 'mdi-calendar',
+      required: false,
+    },
+    placeholder: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    dense: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+    format: {
+      type: String,
+      default: 'YYYY/MM/dd HH:mm',
+      required: false,
+    },
+  },
+  data() {
+    return {
+      formatedDate: '',
+      dialog: false,
+      dialogDetail: {},
+    };
+  },
+  computed: {
+    getDate: {
+      get() {
+        return this.dateTime;
+      },
+      set(newValue) {
+        this.$emit('update:dateTime', newValue);
+      },
+    },
+  },
+  created() {
+    this.updateDate(this.dateTime);
+    // console.log(d.generateMonthWeekTable([], true));
+  },
+  methods: {
+    formatTime() {},
+    updateDate(date) {
+      const d = new AasaamDateTime(this.dateTime, this.lang);
+      this.dialogDetail.generatedTable = d.generateMonthWeekTable([], false);
+      console.log(d.generateMonthWeekTable([], true));
+
+      if (this.dateTime) {
+        this.formatedDate = d.isoFormat(this.format);
+        this.dialogDetail.title = d.isoFormat('EEEE d MMMM YYYY HH:mm');
+      }
+    },
+    clearDate() {
+      this.getDate = null;
+      this.formatedDate = '';
+      this.dialogDetail.title = '';
+    },
   },
 };
 </script>
--- app/frontend/pages/index.vue
+++ app/frontend/pages/index.vue
@@ -1,10 +1,45 @@
 <template>
   <v-container class="px-0" fluid>
-    <v-switch
+    <!-- <v-switch
       v-model="switch1"
       :label="`Switch 1: ${switch1.toString()}`"
-    ></v-switch>
+    ></v-switch> -->
     <LangSwitcher />
+    <v-row>
+      <v-col cols="12">
+        <div class="ma-4">
+          date1:{{ selectDate1 }}
+          <DateTimePicker
+            :date-time.sync="selectDate1"
+            :lang="selectLang"
+            placeholder="select Date"
+            :dense="true"
+          />
+        </div>
+        <hr />
+        <div class="ma-4">
+          date2: {{ selectDate2 }}
+          <br />
+          <DateTimePicker
+            :date-time.sync="selectDate2"
+            :lang="selectLang"
+            placeholder="انتخاب زمان"
+          />
+        </div>
+        <hr />
+        <div class="ma-4">
+          date3:
+          <br />
+          <pre>{{ selectDate3 }}</pre>
+          <DateTimePicker
+            :date-time.sync="selectDate3"
+            :lang="selectLang"
+            format="EEEE d MMMM YYYY HH:mm"
+            placeholder="انتخاب زمان"
+          />
+        </div>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -13,6 +48,10 @@ export default {
   data() {
     return {
       switch1: this.$vuetify.rtl,
+      selectDate1: undefined,
+      selectDate2: new Date(),
+      selectDate3: new Date('1986-01-27 12:00:00'),
+      selectLang: this.$i18n.locale,
     };
   },
   // mounted() {
